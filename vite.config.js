858e98f0b92a39ae67203c6d36eb530d9a92f773
Khanh Duy <duyngonguyenkhanh@gmail.com>
2024-12-28 14:41:53 +0700

    fix github page

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/booking-app-client/', 
   plugins: [react()],
   optimizeDeps: {
     include: ['jwt-decode'],
@@ -10,5 +11,6 @@ export default defineConfig({
   server: {
     port: 3000, // Thay 3000 bằng số cổng bạn muốn sử dụng
   },
+ 
 })
 
