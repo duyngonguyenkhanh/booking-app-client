94c8b5f4524fc42f7168bbdbccb206ef58b429ba
Khanh Duy <duyngonguyenkhanh@gmail.com>
2024-12-31 11:03:57 +0700

    fix router

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  
   plugins: [react()],
+  base: '/booking-app-client/',  // Đảm bảo base được đặt ở đây
   optimizeDeps: {
     include: ['jwt-decode'],
-    base: '/booking-app-client/',
   },
   server: {
     port: 3000, // Thay 3000 bằng số cổng bạn muốn sử dụng
   },
- 
 })
-
