50fee812182dcd95af8f94c2de8669dc6fc37c4d
Khanh Duy <duyngonguyenkhanh@gmail.com>
2024-12-31 11:35:14 +0700

    fix error img

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -4,21 +4,21 @@ const Featured = ({properties}) => {
   return (
     <div className="featured">
       <div className="featuredItem">
-        <img src="../../../public/Ha Noi.jpg" alt="" className="featuredImg" />
+        <img src="/Ha Noi.jpg" alt="" className="featuredImg" />
         <div className="featuredTitles">
           <h1>Hà Nội</h1>
           <h2>{properties.hanoi} properties</h2>
         </div>
       </div>
       <div className="featuredItem">
-        <img src="../../../public/HCM.jpg" alt="" className="featuredImg" />
+        <img src="/HCM.jpg" alt="" className="featuredImg" />
         <div className="featuredTitles">
           <h1>Hồ Chí Minh</h1>
           <h2>{properties.hochiminh} properties</h2>
         </div>
       </div>
       <div className="featuredItem">
-        <img src="../../../public/Da Nang.jpg" alt="" className="featuredImg" />
+        <img src="/Da Nang.jpg" alt="" className="featuredImg" />
         <div className="featuredTitles">
           <h1>Đà Nẵng</h1>
           <h2>{properties.danang} properties</h2>
